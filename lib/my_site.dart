4a164625abeeb519ac235aeee21d19639e6ad3d7
Romario <lromarioyunior@gmail.com>
2023-02-27 06:36:44 -0500

    l10n

--- lib/my_site.dart
+++ lib/my_site.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:portafolios/core/configs/connection/bloc/connected_bloc.dart';
 import 'package:portafolios/core/configs/connection/network_check.dart';
 import 'package:portafolios/core/providers/drawer_provider.dart';
 import 'package:portafolios/core/providers/scroll_provider.dart';
 import 'package:portafolios/core/theme/app_theme.dart';
 import 'package:portafolios/core/theme/cubit/theme_cubit.dart';
+import 'package:portafolios/src/presentation/l10n/app_localizations.dart';
+import 'package:portafolios/src/presentation/l10n/support_locale.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
+
+import 'src/presentation/l10n/locale_provider.dart';
 
 class MySite extends StatelessWidget {
   const MySite({Key? key}) : super(key: key);
@@ -26,16 +31,35 @@ class MySite extends StatelessWidget {
         ],
         child: BlocBuilder<ThemeCubit, ThemeState>(builder: (context, state) {
           return Sizer(builder: (context, orientation, deviceType) {
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'Sudesh',
-              theme: AppTheme.themeData(state.isDarkThemeOn, context),
-              initialRoute: "/",
-              routes: {"/": (context) => const NChecking()},
-            );
+            return ChangeNotifierProvider(
+                create: (context) => LocaleProvider(),
+                builder: (context, child) {
+                  return Consumer<LocaleProvider>(
+                      builder: (context, provider, child) {
+                    return MaterialApp(
+                      localizationsDelegates: const [
+                        GlobalMaterialLocalizations.delegate,
+                        GlobalWidgetsLocalizations.delegate,
+                        GlobalCupertinoLocalizations.delegate,
+                      ],
+                      locale: provider.locale,
+                      supportedLocales: L10n.support,
+                      debugShowCheckedModeBanner: false,
+                      title: AppLocalizations.of(context).companyName,
+                      theme: AppTheme.themeData(state.isDarkThemeOn, context),
+                      initialRoute: "/",
+                      routes: {"/": (context) => const NChecking()},
+                    );
+                  });
+                }
+
+);
           });
         }),
       ),
     );
   }
 }
+
+
+
